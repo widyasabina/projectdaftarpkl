ca0fb049fd5eb6fecceeebcacb4492efa8b704ca
widyasabina <210323024@fellow.lpkia.ac.id>
2023-08-28 10:25:05 +0700

    logo, nambah link sosmed

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -13,7 +13,7 @@ const Hero = () => {
               <div
                 className="wow fadeInUp mx-auto max-w-[800px] text-center"
                 data-wow-delay=".2s"
-              >
+              > &nbsp;
                 <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
                 Sistem Informasi Pendaftaran 
                 </h1>
